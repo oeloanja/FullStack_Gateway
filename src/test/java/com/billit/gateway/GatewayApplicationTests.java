57978afbc9e7a7328e0ca20c77ab90e9593e6980
platinum57 <sook8986@naver.com>
2024-12-08 19:22:09 +0900

    first commit

---
+++ src/test/java/com/billit/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billit.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
